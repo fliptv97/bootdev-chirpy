29af78559fbbacd0d2028f171504a6fdf09104e7
Philip Kazakov <fliptv97@gmail.com>
2025-02-20 14:22:46 +0200

    feat: improve chirps validation, add reset db and create user api methods

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/fliptv97/bootdev-chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
 }
 
 func main() {
+	godotenv.Load()
+
 	const filepathRoot = "."
 	const port = "8080"
 
-	apiCfg := apiConfig{}
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		log.Fatalf("Database connection error: %s", err)
+	}
+
+	apiCfg := apiConfig{
+		db:       database.New(db),
+		platform: os.Getenv("PLATFORM"),
+	}
+
 	mux := http.NewServeMux()
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	mux.HandleFunc("GET /api/healthz", handleReadiness)
 	mux.HandleFunc("POST /api/validate_chirp", handleChirpValidation)
+	mux.HandleFunc("POST /api/users", apiCfg.handleCreateUser)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handleReset)
@@ -63,10 +85,25 @@ func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte(layout))
 }
 
-func (cfg *apiConfig) handleReset(w http.ResponseWriter, _ *http.Request) {
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed on dev environment"))
+
+		return
+	}
+
+	err := cfg.db.RemoveAllUsers(r.Context())
+
+	if err != nil {
+		responseWithError(w, http.StatusInternalServerError, "Can't remove all users", err)
+
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0 and database reset to initial state"))
 }
